d987e9c9e6e9dfed9d0ff98c95fc030bfb4ebb33
Paul Sowden <paulsowden@google.com>
2025-10-28 09:25:16 -0700

    Fix a bug matching "likely" class names
    Previously the logic would allow "Java identifier parts" to follow package separator `.`, this would allow strings like `1.2` to match as "likey class names".
    
    Change-Id: I743bbe0d9395242b4ddae502cf9eea79089db0eb

--- lint/libs/lint-api/src/main/java/com/android/tools/lint/client/api/ResourceVisitor.kt
+++ lint/libs/lint-api/src/main/java/com/android/tools/lint/client/api/ResourceVisitor.kt
@@ -133,7 +133,7 @@ internal class ResourceVisitor(
           // ok
         } else if (prev == '.' && c.isJavaIdentifierStart()) {
           // ok
-        } else if (c.isJavaIdentifierPart()) {
+        } else if (prev != '.' && c.isJavaIdentifierPart()) {
           // ok
         } else {
           return false
